d3c913ccd2482208aa6e686e3a75b02c91e615a6
Elias Roussos <eliasIsaiah@outlook.com>
2019-07-16 12:43:36 -0400

    -logic written to randomly generate numbers for: ----targetNumber ----truffle var numbers(aka the crystal values) -logic tested and is functional

---
+++ assets/javascript/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    let game = {
+
+        /* 
+        
+        VARIABLES:
+
+        -random number
+            --target number
+            --truffle1 number
+            --truffle2 number
+            --truffle3 number
+            --truffle4 number        
+        
+        */
+
+        targetNumber: null,
+        truffle0: null,
+        truffle1: null,
+        truffle2: null,
+        truffle3: null,
+        truffles: [],
+        wins: 0,
+        losses: 0,
+        score: 0,
+
+        getRandom: function (min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+        },
+
+        generateTarget: function () {
+            this.targetNumber = this.getRandom(100, 40);
+            console.log(`targetNumber: ${game.targetNumber}`);
+        },
+
+        generateTruffles: function() {
+            while(this.truffles.length < 4) {
+
+                let rand = this.getRandom(30, 10);
+
+                if(this.truffles.indexOf(rand) === -1) {
+                    this.truffles.push(rand);
+                }
+            }
+            console.log(this.truffles);
+        }
+    }
+
+    game.generateTruffles();
+
+});
